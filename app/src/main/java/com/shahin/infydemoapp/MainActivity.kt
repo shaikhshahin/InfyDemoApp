438d6131b08e3496fe5ccf9137e31b385df07cff
Apple <apple@Apples-MacBook-Pro.local>
2019-11-07 15:14:42 +0530

    first commit

--- app/src/main/java/com/shahin/infydemoapp/MainActivity.kt
+++ app/src/main/java/com/shahin/infydemoapp/MainActivity.kt
@@ -3,6 +3,9 @@ package com.shahin.infydemoapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
+
+//MAIn
+
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
